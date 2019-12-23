2d092c5b0037c8f19a14832b3e015bf753aab158
Arlidio Dobra <lidio@live.co.uk>
2019-12-23 02:37:41 +0000

    MVVM Foundation

---
+++ Appointment_Mgr/Bootstrapper.cs
@@ -0,0 +1,35 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Windows;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Appointment_Mgr.ViewModels;
+
+namespace Appointment_Mgr
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            //Initialize will start up processes
+            Initialize();
+        }
+
+        /// <summary>
+        /// Overrides "OnStartup" method to display root view, defining startup window - in this case ShellViewModel
+        /// Method is calling the ViewModel "ShellView" in ViewModels.
+        /// Basically defining the onstartup window.
+        /// 
+        /// Upon startup, ShellViewModel is called - because of Caliburn.Micro, the View ShellView.xaml is opened
+        /// instead of the ShellViewModel, due to Naming Conventions
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+    }
+}
---
+++ Appointment_Mgr/ViewModels/ShellViewModel.cs
@@ -0,0 +1,34 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Appointment_Mgr.ViewModels
+{
+    /// <summary>
+    /// Inheriting from screen allows for control over opening / closing events.
+    /// I.E. in event where form is updated but not yet saved, screen would allow for
+    /// warning to enduser if they would like to save before exiting - JUST ONE OF MANY EXAMPLES
+    /// </summary>
+    public class ShellViewModel : Screen
+    {   
+        // naming convention : start with _ then camelCase. These variables should not be manipulated directly
+        private string _welcomeMessage = "Welcome";
+
+        public string WelcomeMessage
+        {
+            get { return _welcomeMessage; }
+            set 
+            {
+                _welcomeMessage = value;
+                NotifyOfPropertyChange(() => WelcomeMessage); // Notify anyone who cares about _welcomeMessage that the property has been changed.
+            }
+
+        }
+
+
+
+    }
+}
