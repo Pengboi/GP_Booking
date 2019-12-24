62d2928ffc32f15a2beddd787c273edae31b8912
Arlidio Dobra <lidio@live.co.uk>
2019-12-24 01:08:23 +0000

    UI Mockup & Assets

--- Appointment_Mgr/ViewModels/ShellViewModel.cs
+++ Appointment_Mgr/ViewModels/ShellViewModel.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Threading;
 
 namespace Appointment_Mgr.ViewModels
 {
@@ -15,20 +17,75 @@ namespace Appointment_Mgr.ViewModels
     public class ShellViewModel : Screen
     {   
         // naming convention : start with _ then camelCase. These variables should not be manipulated directly
-        private string _welcomeMessage = "Welcome";
+        private string _greetingMessage = "Good " + getGreeting() + "."; private string _loginMessage = "Login";
+        private string _liveClock = DateTime.Now.ToString("HH:mm"); private string _liveDate = DateTime.Now.ToString("dd/MM/yy");
 
-        public string WelcomeMessage
+        public string GreetingMessage
         {
-            get { return _welcomeMessage; }
+            get { return _greetingMessage; }
             set 
             {
-                _welcomeMessage = value;
-                NotifyOfPropertyChange(() => WelcomeMessage); // Notify anyone who cares about _welcomeMessage that the property has been changed.
+                _greetingMessage = value;
+                NotifyOfPropertyChange(() => GreetingMessage); // Notify anyone who cares about _welcomeMessage that the property has been changed.
             }
 
         }
 
+        public string LoginMessage 
+        {
+            get { return _loginMessage; }
+            set
+            {
+                _loginMessage = value;
+                NotifyOfPropertyChange(() => LoginMessage);
+            }
 
+        }
+
+       public string LiveClock 
+       {
+            get { return _liveClock; }
+            set { _liveClock = value; }
+       }
+
+        public string LiveDate
+        {
+            get { return _liveDate; }
+            set { _liveDate = value; }
+        }
+
+        public static string getGreeting() 
+        {
+            TimeSpan morning = new TimeSpan(10, 0, 0);
+            TimeSpan afternoon = new TimeSpan(12, 0, 0);
+            TimeSpan now = DateTime.Now.TimeOfDay;
+
+            if ((now > morning) && (now < afternoon))
+            {
+                return "Morning";
+            }
+            else 
+            {
+                return "Afternoon";
+            }            
+        }
+        void updateTimeComponents(object sender, EventArgs e)
+        {
+            LiveClock = DateTime.Now.ToString("HH:mm");
+            LiveDate = DateTime.Now.ToString("dd/MM/yy");
+            GreetingMessage = "Good " + getGreeting() + ".";
+        }
+
+        public ShellViewModel() 
+        {
+            DispatcherTimer timer = new DispatcherTimer(DispatcherPriority.Render);
+            timer.Interval = TimeSpan.FromMilliseconds(1);
+            timer.Tick += updateTimeComponents;
+            timer.Start();
+
+        }
+
+        
 
     }
 }
