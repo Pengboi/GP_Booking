a160e41214ce4fff24360dc7678ecf04f77ef1fc
Arlidio Dobra <lidio@live.co.uk>
2020-01-11 00:08:28 +0000

    For later use

---
+++ Appointment_Mgr/Dialog/DialogBoxView.xaml.cs
@@ -0,0 +1,37 @@
+﻿using GalaSoft.MvvmLight.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Appointment_Mgr.Dialog
+{
+    /// <summary>
+    /// Interaction logic for ErrorMessageTemplate.xaml
+    /// </summary>
+    public partial class DialogBoxView : Window
+    {
+        public DialogBoxView()
+        {
+            InitializeComponent();
+            Messenger.Default.Register<NotificationMessage>(this, CloseDialog);
+        }
+
+        private void CloseDialog(NotificationMessage msg)
+        {
+            if (msg.Notification == "CloseDialog")
+            {
+                this.Close();
+            }
+        }
+    }
+}
---
+++ Appointment_Mgr/Dialog/DialogBoxViewModel.cs
@@ -0,0 +1,37 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Messaging;
+using System;
+using System.ComponentModel;
+using System.Windows.Threading;
+using Appointment_Mgr.Model;
+using System.Security;
+using GalaSoft.MvvmLight.Command;
+
+namespace Appointment_Mgr.Dialog
+{
+    public class DialogBoxViewModel : ViewModelBase
+    {
+
+        public DialogBoxViewModel(string title, string message) 
+        {
+            Title = title;
+            Message = message;
+            ButtonClick = new RelayCommand(CloseDialog);
+        }
+
+        public void CloseDialog() 
+        {
+            MessengerInstance.Send<NotificationMessage>(new NotificationMessage("CloseDialog"));
+        }
+
+        public string Title 
+        { get; set; }
+        public string Message
+        { get; set; }
+        public string MessageImage 
+        { get; set; }
+        public string ButtonText
+        { get; set; }
+        public RelayCommand ButtonClick { private set; get; }
+    }
+}
