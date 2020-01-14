16bfb06f922660618f25c53512fa8bfcc5c9a221
Arlidio Dobra <lidio@live.co.uk>
2020-01-14 21:27:14 +0000

    Helps OTP Dialog box move to next UI element on keyup event  for  UX XP adjustments

---
+++ Appointment_Mgr/Helper/TabOnInputBehaviour.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Interactivity;
+
+namespace Appointment_Mgr.Helper
+{
+    public class TabOnInputBehavior : Behavior<TextBox>
+    {
+
+        protected override void OnAttached()
+        {
+            AssociatedObject.PreviewKeyUp += AssociatedObject_PreviewKeyUp;
+        }
+
+        private void AssociatedObject_PreviewKeyUp(object sender, KeyEventArgs e)
+        {
+            if ((e.Key >= Key.D0 && e.Key <= Key.D9) || (e.Key >= Key.NumPad0 && e.Key <= Key.NumPad9))
+            {
+                var request = new TraversalRequest(FocusNavigationDirection.Next);
+                request.Wrapped = true;
+                AssociatedObject.MoveFocus(request);
+            }
+        }
+
+        protected override void OnDetaching()
+        {
+            AssociatedObject.PreviewKeyDown -= AssociatedObject_PreviewKeyUp;
+        }
+
+    }
+}
