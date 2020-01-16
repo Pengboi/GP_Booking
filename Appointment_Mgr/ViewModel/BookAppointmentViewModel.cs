ba3b0610b5f5bb1d0d8d655fe9e837f5a18692f9
pengboi <pengboi@DESKTOP-884FS1A>
2020-01-16 10:18:35 +0000

    Book Appointment View + VM created

---
+++ Appointment_Mgr/ViewModel/BookAppointmentViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Appointment_Mgr.ViewModel
+{
+    public class BookAppointmentViewModel : MainViewModel
+    {
+
+        public BookAppointmentViewModel() 
+        {
+        }
+    }
+}
