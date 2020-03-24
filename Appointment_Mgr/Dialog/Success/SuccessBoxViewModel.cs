b82aba82ede9cc96b718c592904a3f5f94cdca7b
Arlidio Dobra <lidio@live.co.uk>
2020-03-24 18:48:09 +0000

    Replaces Confirmation Dialog box

---
+++ Appointment_Mgr/Dialog/Success/SuccessBoxViewModel.cs
@@ -0,0 +1,31 @@
+﻿using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Appointment_Mgr.Dialog
+{
+    public class SuccessBoxViewModel : DialogBoxViewModelBase<DialogResults>
+    {
+
+        public ICommand OKCommand { get; private set; }
+        public string Message { get; set; }
+        public string Title { get; set; }
+
+
+        public SuccessBoxViewModel(string title, string message) : base(title, message)
+        {
+            OKCommand = new RelayCommand<IDialogWindow>(OK);
+            Message = message;
+            Title = title;
+        }
+
+        private void OK(IDialogWindow window)
+        {
+            CloseDialogWithResult(window, "OK");
+        }
+    }
+}
