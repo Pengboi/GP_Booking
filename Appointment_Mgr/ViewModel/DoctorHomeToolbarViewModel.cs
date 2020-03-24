b75ea2eaeefea6bcfad63db2d84efd522e580a39
Arlidio Dobra <lidio@live.co.uk>
2020-03-24 18:53:44 +0000

    Presentation Layer logic for Doctor Sub-system

---
+++ Appointment_Mgr/ViewModel/DoctorHomeToolbarViewModel.cs
@@ -0,0 +1,92 @@
+﻿using Appointment_Mgr.Model;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using GalaSoft.MvvmLight.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+
+namespace Appointment_Mgr.ViewModel
+{
+    public class DoctorHomeToolbarViewModel : ViewModelBase
+    {
+        private string _userLoggedIn;
+        private string _clockTime = DateTime.Now.ToString("HH:mm"); private string _dateValue = DateTime.Now.ToString("dd/MM/yy");
+        private DispatcherTimer timer;
+
+        public string UserLoggedIn
+        {
+            get { return _userLoggedIn; }
+            set 
+            {
+                value = _userLoggedIn;
+                RaisePropertyChanged(nameof(UserLoggedIn));
+            }
+        }
+
+        #region Clock elements
+        public string LiveClock
+        {
+            get { return this._clockTime; }
+            set
+            {
+                if (this._clockTime == value)
+                    return;
+                this._clockTime = value;
+                RaisePropertyChanged(nameof(LiveClock));
+            }
+        }
+        public string LiveDate
+        {
+            get { return this._dateValue; }
+            set
+            {
+                if (this._dateValue == value)
+                    return;
+                this._dateValue = value;
+                RaisePropertyChanged(nameof(LiveDate));
+            }
+        }
+        #endregion
+
+        public RelayCommand ExecuteLogout { get; }
+
+        public DoctorHomeToolbarViewModel() 
+        {
+            if (IsInDesignMode) 
+            {
+                LiveClock = "15:45";
+                LiveDate = "05/09/16";
+                UserLoggedIn = "Dr. Page Madison";
+            }
+            timer = new DispatcherTimer(DispatcherPriority.Normal);
+            timer.Interval = TimeSpan.FromSeconds(0.3);
+            timer.Tick += (sender, args) =>
+            {
+                LiveClock = DateTime.Now.ToString("HH:mm");
+                LiveDate = DateTime.Now.ToString("dd/MM/yy");
+            };
+            timer.Start();
+
+            Messenger.Default.Register<int>(this, SetUserLoggedIn);
+            UserLoggedIn = _userLoggedIn;
+            Console.WriteLine("SO I GOT IT THO: " + _userLoggedIn);
+            ExecuteLogout = new RelayCommand(ExecuteLogoutCommand);
+        }
+
+        private void SetUserLoggedIn(int id)
+        {
+            _userLoggedIn = StaffDBConverter.GetDoctorNameByID(id);
+        }
+
+        private void ExecuteLogoutCommand()
+        {
+            MessengerInstance.Send<string>("HomeView");
+            MessengerInstance.Unregister(this); // moves messenger to garbage collection
+            ViewModelLocator.Cleanup();
+        }
+    }
+}
---
+++ Appointment_Mgr/ViewModel/DoctorHomeViewModel.cs
@@ -0,0 +1,124 @@
+﻿using Appointment_Mgr.Model;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using GalaSoft.MvvmLight.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+
+namespace Appointment_Mgr.ViewModel
+{
+    public class DoctorHomeViewModel : ViewModelBase
+    {
+        private DispatcherTimer timer;
+        private string _greetingMessage, _doctorName;
+
+        public string GreetingMessage 
+        {
+            get { return _greetingMessage; }
+            set 
+            {
+                _greetingMessage = value;
+                RaisePropertyChanged(nameof(GreetingMessage));
+            }
+        }
+
+
+        public string DoctorName 
+        {
+            get { return _doctorName; }
+            set 
+            {
+                _doctorName = value;
+                RaisePropertyChanged(nameof(DoctorName));
+            }
+        }
+
+        private int DoctorID { get; set; }
+
+        private RelayCommand StartAppointmentCommand { get; set; }
+
+        public DoctorHomeViewModel() 
+        {
+            if (IsInDesignMode)
+            {
+                GreetingMessage = "Good Morning,";
+            }
+            GreetingMessage = "Welcome Back,";
+
+            timer = new DispatcherTimer(DispatcherPriority.Render);
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += (sender, args) =>
+            {
+                GreetingMessage = getGreeting();
+            };
+            timer.Start();
+
+            Messenger.Default.Register<int>(this, SetDoctorDetails);
+            StartAppointmentCommand = new RelayCommand(StartAppointment);
+        }
+
+        public static string getGreeting()
+        {
+            TimeSpan morning = new TimeSpan(0, 0, 0);
+            TimeSpan afternoon = new TimeSpan(12, 0, 0);
+            TimeSpan now = DateTime.Now.TimeOfDay;
+
+            if ((now > morning) && (now < afternoon))
+            {
+                return "Good Morning,";
+            }
+            else
+            {
+                return "Good Afternoon,";
+            }
+        }
+
+        public void SetDoctorDetails(int msg) 
+        {
+            DoctorID = msg;
+            DoctorName = StaffDBConverter.GetDoctorNameByID(DoctorID);
+        }
+
+        private void StartAppointment()
+        {
+
+            DataTable CheckedInPatients = PatientDBConverter.GetCheckedInAppointments();
+            int averageDuration = AppointmentLogic.GetAverage();
+            int remainingShiftInMinutes = StaffDBConverter.GetRemainingShiftInMinutes(DoctorID);
+
+            if (remainingShiftInMinutes < averageDuration) 
+            {
+                //insert alert and return if answer is "NO"
+            }
+            
+
+
+            /*
+             * 
+             * Calculate if doctor has enough time in shift for next appointment
+             *      // if not, ask doctor to confirm they wish to take appointment
+             * Fetch Appointment (to assign)
+             *    {   
+             *         IF Emergency is waiting:
+             *              based on time emergency was created (in scenario there are several emergency appointments waiting) assign one waiting longest
+             *              [this will impact reservation appointment wait time of the doctors]
+             *         
+             *         ELSE IF Doctor A has no RESERVATION appointment for him but there is a walk-in appointment for doctor B that doctor A can see early:
+             *              asked if doctor A would like to take over next appointment --> gets reassigned from doctor B to doctor A
+             *         
+             *         ELSE IF Doctor has reservation waiting for him at same time (or within 10 minutes of) walk-in appointment, reservation appointment takes priority
+             *                 and doctor is assigned reservation
+             *        
+             *         if there is no one waiting doctor is told no patients are checked in yet.
+             *
+             *    }
+             */
+            throw new NotImplementedException();
+        }
+    }
+}
