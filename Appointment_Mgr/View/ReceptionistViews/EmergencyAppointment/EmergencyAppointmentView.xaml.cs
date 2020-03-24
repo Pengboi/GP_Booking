658ad592a9bfd0d14273f875e8a57ebf58021217
Arlidio Dobra <lidio@live.co.uk>
2020-03-24 18:50:21 +0000

    Implemented Emergency Appointment View + VM

---
+++ Appointment_Mgr/View/ReceptionistViews/EmergencyAppointment/EmergencyAppointmentView.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Appointment_Mgr.View
+{
+    /// <summary>
+    /// Interaction logic for EmergencyAppointmentView.xaml
+    /// </summary>
+    public partial class EmergencyAppointmentView : UserControl
+    {
+        public EmergencyAppointmentView()
+        {
+            InitializeComponent();
+        }
+
+        private void DatePicker_CalendarOpened(object sender, RoutedEventArgs e)
+        {
+            var datepicker = sender as DatePicker;
+            if (datepicker != null)
+            {
+                var popup = datepicker.Template.FindName("PART_Popup", datepicker) as Popup;
+                if (popup != null && popup.Child is Calendar)
+                    ((Calendar)popup.Child).DisplayMode = CalendarMode.Decade;
+            }
+        }
+    }
+}
